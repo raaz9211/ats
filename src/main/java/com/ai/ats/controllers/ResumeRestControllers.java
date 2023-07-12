c0825e970fc30becc30ee8f9ecacf54c5367c62c
Raaz <theraj1999@gmail.com>
2023-07-12 22:43:56 +0530

    Second, ExceptionHandler

--- src/main/java/com/ai/ats/controllers/ResumeRestControllers.java
+++ src/main/java/com/ai/ats/controllers/ResumeRestControllers.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-        import java.util.List;
+import java.util.List;
 
 
 
